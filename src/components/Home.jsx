f9f16e307b5b337e0641f72ef2d9a1fdd3327457
satyam <mishrasatyam9890@gmail.com>
2024-06-19 21:43:06 +0530

    Carousel BG Animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,42 +5,87 @@ import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 
 const Home = () => {
+    
     return (
         <>
             <div className="home">
-                <div className="home-bg w-screen h-screen">
-                    <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[100%] h-[100%] object-cover' />
-                </div>
-                <div className="home-headings w-[40%] absolute top-[20%] left-[8%] flex flex-col gap-6">
-                    <h2 className='text-5xl font-extrabold text-[#fd1212]'>Movie Name</h2>
-                    <h3 className='font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam suscipit harum? Iure soluta aut illo tenetur iusto, voluptatem quasi inventore voluptas dignissimos aliquam assumenda eveniet, eum possimus delectus rerum ratione magni unde harum amet dolores quia, esse ullam? Esse.</h3>
-                    <div className="heading-btn flex gap-4">
-                        <button className='bg-[#fd1212] flex items-center gap-1 text-white rounded-lg py-2 px-4'><CiPlay1 className='text-xl' /> Watch Now</button>
-                        <button className='bg-[#fd121230] flex items-center gap-1 text-white border-2 border-solid border-black rounded-lg py-2 px-4'><CiBookmark className='text-xl' />Watch Later</button>
+                <div className="home-list">
+                    <div className='home-item absolute inset-[0 0 0 0] z-10'>
+                        <div className="home-bg w-screen h-screen">
+                            <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[100%] h-[100%] object-cover' />
+                        </div>
+                        <div className="home-headings w-[40%] absolute top-[20%] left-[8%] flex flex-col gap-6">
+                            <h2 className='text-5xl font-extrabold text-[#fd1212]'>Movie Name</h2>
+                            <h3 className='font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam suscipit harum? Iure soluta aut illo tenetur iusto, voluptatem quasi inventore voluptas dignissimos aliquam assumenda eveniet, eum possimus delectus rerum ratione magni unde harum amet dolores quia, esse ullam? Esse.</h3>
+                            <div className="heading-btn flex gap-4">
+                                <button className='bg-[#fd1212] flex items-center gap-1 text-white rounded-lg py-2 px-4'><CiPlay1 className='text-xl' /> Watch Now</button>
+                                <button className='bg-[#fd121230] flex items-center gap-1 text-white border-2 border-solid border-black rounded-lg py-2 px-4'><CiBookmark className='text-xl' />Watch Later</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='home-item absolute inset-[0 0 0 0]'>
+                        <div className="home-bg w-screen h-screen">
+                            <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[100%] h-[100%] object-cover' />
+                        </div>
+                        <div className="home-headings w-[40%] absolute top-[20%] left-[8%] flex flex-col gap-6">
+                            <h2 className='text-5xl font-extrabold text-[#fd1212]'>Movie Name</h2>
+                            <h3 className='font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam suscipit harum? Iure soluta aut illo tenetur iusto, voluptatem quasi inventore voluptas dignissimos aliquam assumenda eveniet, eum possimus delectus rerum ratione magni unde harum amet dolores quia, esse ullam? Esse.</h3>
+                            <div className="heading-btn flex gap-4">
+                                <button className='bg-[#fd1212] flex items-center gap-1 text-white rounded-lg py-2 px-4'><CiPlay1 className='text-xl' /> Watch Now</button>
+                                <button className='bg-[#fd121230] flex items-center gap-1 text-white border-2 border-solid border-black rounded-lg py-2 px-4'><CiBookmark className='text-xl' />Watch Later</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='home-item absolute inset-[0 0 0 0]'>
+                        <div className="home-bg w-screen h-screen">
+                            <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[100%] h-[100%] object-cover' />
+                        </div>
+                        <div className="home-headings w-[40%] absolute top-[20%] left-[8%] flex flex-col gap-6">
+                            <h2 className='text-5xl font-extrabold text-[#fd1212]'>Movie Name</h2>
+                            <h3 className='font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam suscipit harum? Iure soluta aut illo tenetur iusto, voluptatem quasi inventore voluptas dignissimos aliquam assumenda eveniet, eum possimus delectus rerum ratione magni unde harum amet dolores quia, esse ullam? Esse.</h3>
+                            <div className="heading-btn flex gap-4">
+                                <button className='bg-[#fd1212] flex items-center gap-1 text-white rounded-lg py-2 px-4'><CiPlay1 className='text-xl' /> Watch Now</button>
+                                <button className='bg-[#fd121230] flex items-center gap-1 text-white border-2 border-solid border-black rounded-lg py-2 px-4'><CiBookmark className='text-xl' />Watch Later</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='home-item absolute inset-[0 0 0 0]'>
+                        <div className="home-bg w-screen h-screen">
+                            <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[100%] h-[100%] object-cover' />
+                        </div>
+                        <div className="home-headings w-[40%] absolute top-[20%] left-[8%] flex flex-col gap-6">
+                            <h2 className='text-5xl font-extrabold text-[#fd1212]'>Movie Name</h2>
+                            <h3 className='font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quisquam suscipit harum? Iure soluta aut illo tenetur iusto, voluptatem quasi inventore voluptas dignissimos aliquam assumenda eveniet, eum possimus delectus rerum ratione magni unde harum amet dolores quia, esse ullam? Esse.</h3>
+                            <div className="heading-btn flex gap-4">
+                                <button className='bg-[#fd1212] flex items-center gap-1 text-white rounded-lg py-2 px-4'><CiPlay1 className='text-xl' /> Watch Now</button>
+                                <button className='bg-[#fd121230] flex items-center gap-1 text-white border-2 border-solid border-black rounded-lg py-2 px-4'><CiBookmark className='text-xl' />Watch Later</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="home-btn absolute top-[75%] left-[8%] flex gap-5 text-2xl">
-                    <button className='bg-[#00000080] text-white p-2 rounded-[50%]'><IoIosArrowBack /></button>
-                    <button className='bg-[#00000080] text-white p-2 rounded-[50%]'><IoIosArrowForward /></button>
-                </div>
-                <div className="home-thumbnail absolute flex gap-6 bottom-4 -right-9">
-                    <div className="home-items flex flex-col items-center">
-                        <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-32 h-40 object-cover rounded-xl' />
+                <div className="home-thumbnail absolute flex gap-6 bottom-4 left-[50%] z-50">
+                    <div className="home-t-items flex flex-col items-center">
+                        <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[150px] h-[180px] object-cover rounded-xl' />
                         <h3>Movie</h3>
                     </div>
-                    <div className="home-items flex flex-col items-center">
-                        <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-32 h-40 object-cover rounded-xl' />
+                    <div className="home-t-items flex flex-col items-center">
+                        <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[150px] h-[180px] object-cover rounded-xl' />
                         <h3>Movie</h3>
                     </div>
-                    <div className="home-items flex flex-col items-center">
-                        <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-32 h-40 object-cover rounded-xl' />
+                    <div className="home-t-items flex flex-col items-center">
+                        <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[150px] h-[180px] object-cover rounded-xl' />
                         <h3>Movie</h3>
                     </div>
-                    <div className="home-items flex flex-col items-center">
-                        <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-32 h-40 object-cover rounded-xl' />
+                    <div className="home-t-items flex flex-col items-center">
+                        <img src="https://images.moneycontrol.com/static-mcnews/2024/06/20240615141314_Untitled-design.png?impolicy=website&width=770&height=431" className='w-[150px] h-[180px] object-cover rounded-xl' />
                         <h3>Movie</h3>
                     </div>
                 </div>
+                <div className="home-btn absolute top-[75%] left-[8%] flex gap-5 text-2xl z-50">
+                    <button className='bg-[#00000080] text-white p-2 rounded-[50%]' id='prev'><IoIosArrowBack /></button>
+                    <button className='bg-[#00000080] text-white p-2 rounded-[50%]' id='next'><IoIosArrowForward /></button>
+                </div>
+                <div className="home-timer"></div>
             </div>
         </>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import navLogo from '../../public/LOOKaSHOW FavIcon.png'
 import { RiSearchLine } from "react-icons/ri";
 import { FaHome } from "react-icons/fa";
 import { BiCameraMovie } from "react-icons/bi";
@@ -8,9 +9,9 @@ import { FaBookmark } from "react-icons/fa";
 const Navbar = () => {
     return (
         <>
-            <nav className='w-screen flex justify-between items-center py-2 px-12 fixed top-0'>
-                <div className="navLogo text-2xl">LOOKaSHOW</div>
-                <ul className='flex gap-12'>
+            <nav className='w-screen flex justify-between items-center px-12 fixed top-0 z-50'>
+                <img src={navLogo} className='w-20'/>
+                <ul className='flex gap-12 py-1 px-6 rounded-xl bg-[#fd121230]'>
                     <li className='flex flex-col items-center cursor-pointer'><FaHome />Home</li>
                     <li className='flex flex-col items-center cursor-pointer'><BiCameraMovie />Movie</li>
                     <li className='flex flex-col items-center cursor-pointer'><MdMovie />Series</li>
@@ -18,7 +19,7 @@ const Navbar = () => {
                 </ul>
                 <div className='flex items-center gap-2'>
                     <input type="search" name="search" placeholder='Search' className='hidden px-4 py-1 border-2 border-solid border-black rounded-lg bg-transparent text-white outline-none'/>
-                    <RiSearchLine className='cursor-pointer text-2xl'/>
+                    <RiSearchLine className='cursor-pointer text-2xl text-yellow-300'/>
                 </div>
             </nav>
         </>
